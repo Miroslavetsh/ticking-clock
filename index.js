dc9b6af84ca2e2fa4a34293e25b6c6e25f850db2
Miroslavetsh <dfgshte@gmail.com>
2023-07-24 18:52:16 +0300

    Add declarative solution

--- index.js
+++ index.js
@@ -1,41 +1,64 @@
-setInterval(logCLock, 1000)
+const compose =
+  (...fns) =>
+  (arg) =>
+    fns.reduce((acc, fn) => fn(acc), arg)
 
-function logCLock() {
-  let time = getTime()
+// utils
+const oneSecond = () => 1000
+const getCurrentTime = () => new Date()
+const clear = () => console.clear()
+const log = (message) => console.log(message)
 
-  console.clear()
-  console.log(time)
+// Business Layer
+const serializeClockTime = (date) => ({
+  hours: date.getHours(),
+  minutes: date.getMinutes(),
+  seconds: date.getSeconds(),
+})
+
+const civilianHours = (clockTime) => ({
+  ...clockTime,
+  hours: clockTime.hours > 12 ? clockTime.hours - 12 : clockTime.hours,
+})
+
+const appendAMPM = (clockTime) => ({
+  ...clockTime,
+  ampm: clockTime.hours < 12 ? 'AM' : 'PM',
+})
+
+//  HOFs
+const display = (target) => (time) => target(time)
+
+const formatClock = (format) => (clockTime) =>
+  format
+    .replace('hh', clockTime.hours)
+    .replace('mm', clockTime.minutes)
+    .replace('ss', clockTime.seconds)
+    .replace('tt', clockTime.ampm)
+
+const prependZero = (key) => (clockTime) => ({
+  ...clockTime,
+  [key]: clockTime[key] < 10 ? '0' + clockTime[key] : clockTime[key],
+})
+
+// Composition of small parts
+const convertToCivilian = (clockTime) => compose(appendAMPM, civilianHours)(clockTime)
+const doubleDigits = (civilianTime) =>
+  compose(prependZero('hours'), prependZero('minutes'), prependZero('seconds'))(civilianTime)
+
+const startTicking = () => {
+  setInterval(
+    compose(
+      clear,
+      getCurrentTime,
+      serializeClockTime,
+      convertToCivilian,
+      doubleDigits,
+      formatClock('hh:mm:ss tt'),
+      display(log),
+    ),
+    oneSecond(),
+  )
 }
 
-function getTime() {
-  let date = new Date()
-  let time
-
-  time = {
-    hours: date.getHours(),
-    minutes: date.getMinutes(),
-    seconds: date.getSeconds(),
-    ampm: 'AM',
-  }
-
-  if (time.hours === 12) {
-    time.ampm = 'PM'
-  } else if (time.hours > 12) {
-    time.ampm = 'PM'
-    time.hours -= 12
-  }
-
-  if (time.hours < 10) {
-    time.hours = '0' + time.hours
-  }
-
-  if (time.minutes < 10) {
-    time.minutes = '0' + time.minutes
-  }
-
-  if (time.seconds < 10) {
-    time.seconds = '0' + time.seconds
-  }
-
-  return time.hours + ':' + time.minutes + ':' + time.seconds + ' ' + time.ampm
-}
+startTicking()
